38e205e146b085571e4420a577a6d24011533a22
nrkk-azuma <tazuma@newrelic.com>
2022-02-18 09:47:25 +0900

    fix

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -12,7 +12,7 @@ class WelcomeController extends Controller
   public function index(Request $request)
   {
       newrelic_add_custom_parameter("user", $request->input('user'));
-      Log::info('welcome called by ' + $request->input('user'), ['test' => 'test1']);
+      Log::info('welcome called by '.$request->input('user'), ['test' => 'test1']);
       AccountService::hoge();
     return view('welcome');
   }
